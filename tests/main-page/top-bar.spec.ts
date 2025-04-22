5e6ca97c30f31cbdc9d2eb1006a719e204596c53
MV <jocat1210@gmail.com>
2025-04-22 15:05:05 +0530

    Added examples for visual and API test

--- tests/main-page/top-bar.spec.ts
+++ tests/main-page/top-bar.spec.ts
@@ -1,4 +1,10 @@
-import { test, expect, Locator } from "@playwright/test";
+import {
+    test,
+    expect,
+    Locator,
+    request,
+    APIRequestContext,
+} from "@playwright/test";
 import { TestHelper } from "../../page-objects/TestHelper";
 import { tabs, dropDowns } from "../constants/tob-bar-elements";
 
@@ -7,74 +13,82 @@ test.describe("Top Bar Elements", () => {
         await page.goto(`${baseURL}`);
     });
 
+    test("Visual test homepage", async ({ page }) => {
+        await expect(page).toHaveScreenshot();
+    });
+
     Object.entries(tabs).forEach(([tabName, colors]) => {
-        test(`mouse hovers on the ${tabName} tab`, async ({ page }) => {
-            let tabLocator: Locator;
-            let tabNormalColor: string | null | undefined;
-            let tabColor: string | null | undefined;
+        test(
+            `mouse hovers on the ${tabName} tab`,
+            { tag: ["@sample1", "@sample2"] },
+            async ({ page }) => {
+                let tabLocator: Locator;
+                let tabNormalColor: string | null | undefined;
+                let tabColor: string | null | undefined;
 
-            if (tabName === "Phone Icon") {
-                tabLocator = page.locator("svg.phone-icon");
-            } else if (tabName === "Log In") {
-                tabLocator = page.getByText("Log In", { exact: true });
-            } else if (tabName === "Nine Square Icon") {
-                tabLocator = page.locator("svg.nineSquareIcon");
-                tabNormalColor = await page.getAttribute(
-                    "svg.nineSquareIcon path",
-                    "fill",
-                );
-                tabColor = tabNormalColor;
-            } else {
-                tabLocator = page.getByRole("link", { name: tabName });
-            }
+                if (tabName === "Phone Icon") {
+                    tabLocator = page.locator("svg.phone-icon");
+                } else if (tabName === "Log In") {
+                    tabLocator = page.getByText("Log In", { exact: true });
+                } else if (tabName === "Nine Square Icon") {
+                    tabLocator = page.locator("svg.nineSquareIcon");
+                    tabNormalColor = await page.getAttribute(
+                        "svg.nineSquareIcon path",
+                        "fill",
+                    );
+                    tabColor = tabNormalColor;
+                } else {
+                    tabLocator = page.getByRole("link", { name: tabName });
+                }
 
-            const testHelper = new TestHelper(page, tabLocator);
-            const classNameForTheme = await testHelper.getMainThemeColor();
-            let textColor: string | undefined;
-            let hoverTextColor: string | undefined;
+                const testHelper = new TestHelper(page, tabLocator);
+                const classNameForTheme = await testHelper.getMainThemeColor();
+                let textColor: string | undefined;
+                let hoverTextColor: string | undefined;
 
-            // Get the text color of the tab
-            if (tabName !== "Nine Square Icon") {
-                tabNormalColor = await testHelper.getElementColor();
-                tabNormalColor = `#${tabNormalColor}`;
-            }
+                // Get the text color of the tab
+                if (tabName !== "Nine Square Icon") {
+                    tabNormalColor = await testHelper.getElementColor();
+                    tabNormalColor = `#${tabNormalColor}`;
+                }
 
-            // Determine the original text color of the tab depending on the theme
-            if (classNameForTheme === "indian_dark") {
-                textColor = colors.normalDark;
-                hoverTextColor = colors.hoverDark;
-            } else {
-                textColor = colors.normalLight;
-                hoverTextColor = colors.hoverLight;
-            }
+                // Determine the original text color of the tab depending on the theme
+                if (classNameForTheme === "indian_dark") {
+                    textColor = colors.normalDark;
+                    hoverTextColor = colors.hoverDark;
+                } else {
+                    textColor = colors.normalLight;
+                    hoverTextColor = colors.hoverLight;
+                }
 
-            // Validate the color of the tab
-            expect(tabNormalColor).toEqual(textColor),
-                "Original tab color is not correct";
+                // Validate the color of the tab
+                expect(tabNormalColor).toEqual(textColor),
+                    "Original tab color is not correct";
 
-            // Hover on the the tab
-            await tabLocator.hover();
+                // Hover on the the tab
+                await tabLocator.hover();
 
-            // Get the text color of the hovered tab
-            const tabHoveredColor = await testHelper.getElementColor();
+                // Get the text color of the hovered tab
+                const tabHoveredColor = await testHelper.getElementColor();
 
-            // Validate the color of the tab when mouse hovers on it
-            expect(`#${tabHoveredColor}`).toEqual(hoverTextColor),
-                "Mouse hover color is not correct";
+                // Validate the color of the tab when mouse hovers on it
+                expect(`#${tabHoveredColor}`).toEqual(hoverTextColor),
+                    "Mouse hover color is not correct";
 
-            // Move the cursor away from the hovered tab
-            await page.mouse.move(0, 0);
+                // Move the cursor away from the hovered tab
+                await page.mouse.move(0, 0);
 
-            // Again get the color of the tab when mouse is NOT hovering on it
-            if (tabName !== "Nine Square Icon") {
-                tabColor = await testHelper.getElementColor();
-                tabColor = `#${tabColor}`;
-            }
+                // Again get the color of the tab when mouse is NOT hovering on it
+                if (tabName !== "Nine Square Icon") {
+                    tabColor = await testHelper.getElementColor();
+                    tabColor = `#${tabColor}`;
+                }
 
-            // Validate the color of the tab when mouse is NOT hovering on it
-            expect(tabColor).toEqual(textColor),
-                "After removing the mouse pointer, original color is not correct";
-        });
+                // Validate the color of the tab when mouse is NOT hovering on it
+                expect(tabColor).toEqual(textColor),
+                    "After removing the mouse pointer, original color is not correct";
+            },
+        );
     });
 
     Object.entries(dropDowns).forEach(([menuName, data]) => {
@@ -126,5 +140,30 @@ test.describe("Top Bar Elements", () => {
 
         // Validate whether the auto suggestion shows 5 suggestions
         expect(await suggestions.all()).toHaveLength(5);
+    });
+});
+
+test.describe("API Testing Example", () => {
+    let apiContext: APIRequestContext;
+
+    test.beforeAll(async () => {
+        apiContext = await request.newContext({
+            ignoreHTTPSErrors: true,
+        });
+    });
+
+    test("get products", async () => {
+        const apiResponse = await apiContext.get(
+            "https://api.india.delta.exchange/v2/products",
+            {
+                headers: { Accept: "application/json" },
+            },
+        );
+
+        // Validate whether response is OK
+        expect(apiResponse.ok).toBeTruthy();
+
+        // Validate whether the staus code is 200
+        expect(apiResponse.status()).toBe(200);
     });
 });
